9b00d940c75c957b22b0e3a869d2e8710cbf5f7b
heikoschmidt1187 <github@heikoschmidt.info>
2021-04-17 20:26:48 +0200

    started implementation

--- day39.py
+++ day39.py
@@ -19,6 +19,70 @@ live cell to bottom-rightmost live cell.
 
 You can represent a live cell with an asterisk (*) and a dead cell with a dot (.).
 """
+import time
+import os
+import sys
+
+N = 20
+board = []
+
+def pretty_print():
+    os.system('clear')
+    global board
+
+    for r in range(len(board)):
+        for c in range(len(board[r])):
+            if board[r][c] == 1:
+                print('*', end = ' ')
+            else:
+                print('.', end = ' ')
+        print()
+        
+def evolution(board):
+    global N
+    next = board.copy()
+    
+    # check each cell
+    for i in range(N):
+        for j in range(N):
+            
+            # check neigbors alive
+            sum_alive = board[(i-1)%N][(j-1)%N] + board[(i-1)%N][j] + board[(i-1)%N][(j+1)%N] + \
+                        board[i][(j-1)%N] + board[i][(j+1)%N] + \
+                        board[(i+1)%N][(j-1)%N] + board[(i+1)%N][j] + board[(i+1)%N][(j+1)%N]
+
+            # update next board
+            if board[i][j] == 1 and (sum_alive < 2 or sum_alive > 3):
+                next[i][j] = 0
+            elif board[i][j] == 0 and sum_alive == 3:
+                next[i][j] = 1
+                
+    return next
+        
+def game_of_life(start_set, steps):
+    global board
+    global N
+
+    # initialize board
+    for i in range(N):
+        row = []
+        for j in range(N):
+            row.append(0)
+        board.append(row)
+        
+    # to the steps, use step 0 for init
+    for cur_step in range(steps):
+        if cur_step == 0:
+            # set life cells
+            for s in start_set:
+                r, c = s
+                board[r][c] = 1
+        else:
+            board = evolution(board)
+
+        pretty_print()
+        time.sleep(1)
 
 if __name__ == '__main__':
-    pass
+    glider = [ (0, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+    game_of_life(glider, 100)
